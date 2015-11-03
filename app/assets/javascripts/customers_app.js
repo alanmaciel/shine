600cf9f1b160e7bceccadbf6541d20d33ad0365c
Alan Maciel <alan@ubernetics.com>
2015-11-03 14:38:21 -0600

    Implementing showing the searchedFor term using Angular

---
+++ app/assets/javascripts/customers_app.js
@@ -0,0 +1,18 @@
+var app = angular.module('customers',[]);
+
+// var CustomerSearchController = function($scope) {
+//   $scope.search = function(searchTerm) {
+//     $scope.searchedFor = searchTerm;
+//   }
+// }
+// 
+// app.controller("CustomerSearchController", [ "$scope", CustomerSearchController]);
+
+app.controller("CustomerSearchController", [
+          '$scope',
+  function($scope) {
+    $scope.search = function(searchTerm) {
+      $scope.searchedFor = searchTerm;
+    }
+  }
+]);
